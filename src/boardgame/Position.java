83cf778b7922a7231bf960afb499833aafdb11cd
Yuri Sabedot Venturin <ysventurin@ucs.br>
2026-06-17 00:17:00 -0300

    feat: add Position class

---
+++ src/boardgame/Position.java
@@ -0,0 +1,29 @@
+package boardgame;
+
+public class Position {
+    private int row;
+    private int col;
+
+    public Position(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setValues(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    @Override
+    public String toString() {
+        return row + ", " + col;
+    }
+}
